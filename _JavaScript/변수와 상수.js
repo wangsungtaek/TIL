1fc219754ba856f11f173513ccf177ab62c41b98
wangsungtaek <tjdxorv@naver.com>
2021-06-10 08:58:07 +0900

    210610

---
+++ _JavaScript/변수와 상수.js
@@ -0,0 +1,12 @@
+let admin;
+let name;
+
+name = "John";
+admin = name;
+
+alert(admin);
+
+let ourPlanetName = "Earth";
+let currentUserName = "John";
+// userName이라 지어주기보다는, 현재 접속 중인 사용자라는 정보를 담고 있는 이름으로
+
