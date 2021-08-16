ab150c858b51a3c115b2bd8b5598f72648c2b58e
wangsungtaek <tjdxorv@naver.com>
2021-08-16 16:37:13 +0900

    백준 10250번 풀이

--- Main.java
+++ Main.java
@@ -1,21 +1,37 @@
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
+
 
 public class Main {
     public static void main(String[] args) throws IOException {
         
-        BufferedReader br = new BufferedReader(new java.io.InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int up = Integer.parseInt(st.nextToken());
-        int down = Integer.parseInt(st.nextToken());
-        int length = Integer.parseInt(st.nextToken());
+        int testCase = Integer.parseInt(st.nextToken());
 
-        int day = (length - down) / (up - down);
-        if((length - down) % (up - down) != 0)
-            day++;
+        int height = 6;
+        int width = 12;
+        int person = 18;
 
-        System.out.println(day);
+        int ho = 0;
+        
+        for(int i = 0; i < testCase; i++) {
+            StringTokenizer test = new StringTokenizer(br.readLine(), " ");
+
+            height = Integer.parseInt(test.nextToken());
+            width = Integer.parseInt(test.nextToken());
+            person = Integer.parseInt(test.nextToken());
+
+            if((person % height) == 0) {
+                ho = ( height * 100 ) + ( person / height );
+            } else {
+                ho = ( person % height * 100 ) + ( person / height ) + 1;
+            }
+
+            System.out.println(ho);
+        }
     }
 }
