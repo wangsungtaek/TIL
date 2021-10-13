1bcf162b057d6d09a7229481c74bbb9a11e0aeae
wangsungtaek <tjdxorv@naver.com>
2021-10-13 23:43:51 +0900

    add 요구사항 확인

--- Main.java
+++ Main.java
@@ -7,10 +7,28 @@ public class Main {
     public static void main(String[] args) throws NumberFormatException, IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuffer bf = new StringBuffer();
 
-        long n = Long.parseLong(st.nextToken());
-        
-        System.out.printf("%d", n);
-        
+        char result = st.nextToken().charAt(0);
+
+        switch(result) {
+            
+            case 'A':
+                bf.append("best!!!");
+                break;
+            case 'B':
+                bf.append("good!!");
+                break;
+            case 'C':
+                bf.append("run!");
+                break;
+            case 'D':
+                bf.append("slowly~");
+                break;
+            default:
+                bf.append("what?");    
+        }
+
+        System.out.printf( "%s", bf.toString() );
     }
 }
