19e7e5970226e30cb5bfab799dc99171c511700e
wangsungtaek <tjdxrov@naver.com>
2022-08-14 22:11:50 +0900

    [ Poimaweb_TypeScript ] -  1장

---
+++ _Poiemaweb/TypeScript/person.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Person = void 0;
+var Person = /** @class */ (function () {
+    function Person(name) {
+        this.name = name;
+    }
+    Person.prototype.sayHello = function () {
+        return "Hello. ".concat(this.name);
+    };
+    return Person;
+}());
+exports.Person = Person;
+var person = new Person("Lee");
+console.log(person.sayHello());
